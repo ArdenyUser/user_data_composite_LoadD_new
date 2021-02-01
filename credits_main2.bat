8446c59ab06c034bd815da167e2d230f788b39e3
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-02-01 15:25:38 -0500

    a

---
+++ credits_main2.bat
@@ -0,0 +1,23 @@
+@echo off
+
+echo Main Code:
+echo Raspberry Pi Foundation
+echo Linux
+sleep 1
+echo LoadD Folder:
+echo ArdenyUser
+sleep 1
+echo Apps:
+echo Python Devs
+echo Botspot - Pi Apps
+echo Pi Apps App Devs
+echo Raspberry Pi Foundation
+echo Thonny IDE Devs
+echo Chromium Devs
+echo Dillo Devs
+echo VLC Devs
+echo Simple Screen Recorder Devs
+sleep 5
+echo Publishers:
+echo ArdenyUser
+sleep 1
