dc3a135785547076a8814026b4a50733285a8d13
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-02-01 15:54:16 -0500

    broken_alert

---
+++ boot-movie-software2.bat
@@ -0,0 +1,4 @@
+@echo off
+
+error&cls
+echo Error: this program and or file, or folder, is not supported in the %CD%BAT aka Batch file format, or code format. Contact us, or suggest a version of this to ArdenyUser via GitHub or at averywstafford@gmail.com and thanks for using!
